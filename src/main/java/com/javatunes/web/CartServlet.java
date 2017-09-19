bc6a8f70cbc7788c512c3f7b50a20682e22d2ac9
Alex_Gushchin <alexandr_gushchin_93@mail.ru>
2017-09-19 11:38:08 +0300

    update cartservlet

--- src/main/java/com/javatunes/web/CartServlet.java
+++ src/main/java/com/javatunes/web/CartServlet.java
@@ -3,8 +3,6 @@ package main.java.com.javatunes.web;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,9 +12,7 @@ import javax.servlet.http.HttpSession;
 import main.java.com.javatunes.util.MusicItem;
 import main.java.com.javatunes.util.SearchUtility;
 
-/**
- * Servlet implementation class CartServlet
- */
+
 public class CartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -60,22 +56,21 @@ public class CartServlet extends HttpServlet {
 	      // Read the parameter from the "Add to Cart" link
 	      String itemID = request.getParameter("itemID");
 
-	      // Session management - create new or get reference to existing
 	      HttpSession session = request.getSession();
 
-	      // Shopping cart management - create new or get reference to existing
+	    
 	      Collection<MusicItem> cart = (Collection<MusicItem>) session.getAttribute("cart");
 	      if (cart == null)  // first visit: session does not have a "cart"
 	      {
-	         cart = new ArrayList<MusicItem>();  // create new cart
-	         session.setAttribute("cart", cart);  // add it to session
+	         cart = new ArrayList<MusicItem>();  
+	         session.setAttribute("cart", cart);  
 	      }
 
-	      // Shopping cart content management (add item if not already in cart)
+	      
 	      if (itemID != null)
 	      {
 	         MusicItem item = SearchUtility.findById(itemID);
-	         if (!cart.contains(item))  // based on equals() method of ItemValue
+	         if (!cart.contains(item))  
 	         {
 	        	 System.out.println("Adding item: " + item);
 	            cart.add(item);
